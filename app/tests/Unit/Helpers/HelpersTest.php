0f006b1f2c62c8760050a9d4c075f6c7f1bac8b9
Majid Akbari <majid.akbari@neocard.fi>
2021-11-14 14:54:36 +0330

    unit tests for Helpers.php

---
+++ app/tests/Unit/Helpers/HelpersTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Unit\Helpers;
+
+use App\Helpers\Helpers;
+use Symfony\Component\VarDumper\Cloner\Stub;
+use Tests\TestCase;
+
+class HelpersTest extends TestCase
+{
+    /**
+     * @group unit
+     */
+    public function testGetClassName_shouldWork(): void
+    {
+        //arrange
+        $instance = new Stub();
+
+        //act
+        $actual = Helpers::getClassName($instance);
+
+        //assert
+        $this->assertSame('Stub', $actual);
+    }
+}
