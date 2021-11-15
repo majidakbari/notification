06f2a6f3e1cda155d7703ef185368db37230d6b0
Majid Akbari <majid.akbari@neocard.fi>
2021-11-15 22:19:23 +0330

    add supervisor to consumer container as a foreground process to keep it up

---
+++ consumer/app/Console/Commands/ListenToRabbitmqConsoleCommand.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class ListenToRabbitmqConsoleCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'rabbitmq:listen';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $this->info('it works');
+        while (true) {
+
+        }
+    }
+}
